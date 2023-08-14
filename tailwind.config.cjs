d2a7ae5d6fdf1402aa70d9010b5f7f88966e73e7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-08-14 13:11:02 +0630

    feat: add tailwind and breeze starter kit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+const defaultTheme = require('tailwindcss/defaultTheme');
+const forms = require('@tailwindcss/forms');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
